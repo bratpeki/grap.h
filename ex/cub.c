dfdf2a5115dfce6aabdb637e02010a9859f909bb
bratpeki <katic.peco@gmail.com>
2021-10-31 22:37:29 +0100

    Changed spaces to tabs in cub.c

--- ex/cub.c
+++ ex/cub.c
@@ -5,25 +5,25 @@
 
 int main() {
 
-       const int count = 100;
-       double *x = (double*)calloc(sizeof(double), count);
-       double *y = (double*)calloc(sizeof(double), count);
+	const int count = 100;
+	double *x = (double*)calloc(sizeof(double), count);
+	double *y = (double*)calloc(sizeof(double), count);
 
-       int ind = 0;
-       for (int i = -count/2; i < count/2; i++) {
-               x[ind] = (double)i;
-               y[ind] = (double)(i*i*i);
-               ind++;
-       }
+	int ind = 0;
+	for (int i = -count/2; i < count/2; i++) {
+		x[ind] = (double)i;
+		y[ind] = (double)(i*i*i);
+		ind++;
+	}
 
-       char *grid = createGraph2D(x, y, count, 22, 12);
+	char *grid = createGraph2D(x, y, count, 22, 12);
 
-       drawGraphInfo(x, y, count, "Cube function y=x^3", "X values", "Y values");
-       drawGraph2D(grid, 22, 12);
+	drawGraphInfo(x, y, count, "Cube function y=x^3", "X values", "Y values");
+	drawGraph2D(grid, 22, 12);
 
-       free(grid);
-       free(x);
-       free(y);
+	free(grid);
+	free(x);
+	free(y);
 
 }
 
